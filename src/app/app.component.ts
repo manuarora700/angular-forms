9d1435562e1fa98dd003c9e885aa3a45997f0292
Manu Arora <mannuarora700@gmail.com>
2020-09-17 12:15:49 +0530

    ngModel binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  defaultQuestion = 'pet';
   @ViewChild('f', {static: false}) signupForm: NgForm;
   suggestUserName() {
     const suggestedName = 'Superuser';
